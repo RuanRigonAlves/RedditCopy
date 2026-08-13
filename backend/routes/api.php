bccae3f7ab77714a2460aac949fe952fe97fd955
Ruan Rigon <ruanrigon2k@gmail.com>
2026-08-13 16:16:34 -0300

    Routes organization

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,47 +5,63 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 
-Route::get('/test', function () {
-    return [
-        'message' => 'RedditCopy API funcionando!'
-    ];
-});
-
+/*
+|--------------------------------------------------------------------------
+| Public routes
+|--------------------------------------------------------------------------
+*/
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/refresh', [AuthController::class, 'refresh']);
 
+
+/*
+|--------------------------------------------------------------------------
+| Public posts
+|--------------------------------------------------------------------------
+*/
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::get('/posts/{post}/comments', [CommentController::class, 'index']);
+
+
+/*
+|--------------------------------------------------------------------------
+| Authenticated routes
+|--------------------------------------------------------------------------
+*/
+
 Route::middleware('auth:api')->group(function () {
+
+    /*
+    |----------------------------------------------------------------------
+    | Authentication
+    |----------------------------------------------------------------------
+    */
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/claims', [AuthController::class, 'claims']);
 
-    Route::get('/admin/test', function (){
-        return response()->json([
-            'message' => 'Voce e administrador!',
-        ]);
-    })->middleware('role:admin');
 
-    Route::get('/posts/create', function () {
-    return response()->json([
-        'message' => 'Você pode criar posts!',
-    ]);
-})->middleware(['auth:api', 'permission:posts.create']);
-});
-
-Route::middleware('auth:api')->group(function (){
-    Route::get('/posts', [PostController::class, 'index']);
+    /*
+    |----------------------------------------------------------------------
+    | Posts
+    |----------------------------------------------------------------------
+    */
     Route::post('/posts', [PostController::class, 'store']);
-    Route::get('/posts/{post}', [PostController::class, 'show']);
     Route::put('/posts/{post}', [PostController::class, 'update']);
     Route::delete('/posts/{post}', [PostController::class,'destroy']);
-});
 
-Route::get('/posts/{post}/comments', [CommentController::class, 'index']);
 
-Route::middleware('auth:api')->group(function () {
+    /*
+    |----------------------------------------------------------------------
+    | Comments
+    |----------------------------------------------------------------------
+    */
     Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
     Route::post('/comments/{comment}/replies', [CommentController::class, 'reply']);
     Route::put('/comments/{comment}', [CommentController::class, 'update']);
     Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
+
+
 });
